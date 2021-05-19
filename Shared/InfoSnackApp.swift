db7f6df5dee98446c449d26023bdb3b52f75bb2b
Dan Hart <dhart94@gmail.com>
2021-05-19 16:02:56 -0500

    Initial Commit

---
+++ Shared/InfoSnackApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfoSnackApp.swift
+//  Shared
+//
+//  Created by Dan Hart on 5/19/21.
+//
+
+import SwiftUI
+
+@main
+struct InfoSnackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
